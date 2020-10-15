78e249ffe246bc2c94a7f3d66fd70e9b1f086a94
KevinUfkes <62268325+KevinUfkes@users.noreply.github.com>
2020-10-15 10:44:43 -0700

    completed all assignment requirements

--- index.js
+++ index.js
@@ -1,33 +1,42 @@
 const express = require('express');
-const bodyParser = require('body-parser');
+// const bodyParser = require('body-parser');
 const fs = require('fs');
 const router = express.Router();
 const app = express();
-// app.use(bodyParser.urlencoded({ extended: false }));
-// app.use(bodyParser.json());
-// const querystring = require('querystring');
-// const Article = require('./users').Article;
+var merge = require('./mergeSort');
 
 router.get(('/home'), (req, res) => {
     res.sendFile(__dirname + "/" + "home.html");
 })
 
-app.get('/users/', (req, res) => {
+app.get('/user/', (req, res) => {
     uid = req.query['uid']
+    res.setHeader('Content-Type', 'application/json');
     fs.readFile(__dirname + '/users.json', 'utf8', (err, data) => {
         data = JSON.parse(data)
         for(let x=0;x<data.length;x++) {
             if(data[x]['id']==uid) {
-                res.end(JSON.stringify(data[x]['id'] + data[x]['name']), ',', '4')
+                var returnObj = {  
+                    "id": data[x].id, 
+                    "name": data[x].name,
+                    "email": data[x].email,
+                    "address": data[x].address.street + ", " + data[x].address.city + ", " + data[x].address.zipcode,
+                    "phone": data[x].phone
+                }
+                res.end(JSON.stringify(returnObj, 0, 4));
             }
         }
     })
 })
 
-
 router.get(('/users/all'), (req, res) => {
+    res.setHeader('Content-Type', 'application/json');
     fs.readFile(__dirname + '/users.json', 'utf8', (err, data) => {
-        res.end(data, null, '\t');
+        data = JSON.parse(data);
+        data.sort(function (a, b) {
+            return a.username.localeCompare(b.username);
+        });
+        res.end(JSON.stringify(data, 0, 4));
     });
 })
 
